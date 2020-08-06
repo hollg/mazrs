3c1365c55506c0195d2afd71c862f6b09793ffae
Gary Holland <garyhollandxyz@gmail.com>
2020-08-06 21:46:58 +0100

    add enums for args

--- src/main.rs
+++ src/main.rs
@@ -6,26 +6,58 @@ use clap::App;
 use generate::binary_tree::binary_tree;
 use generate::sidewinder::sidewinder;
 use grid::Grid;
+use std::str::FromStr;
 
+enum Algorithm {
+    Binary,
+    Sidewinder,
+}
+
+impl FromStr for Algorithm {
+    type Err = &'static str;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "binary" => Ok(Algorithm::Binary),
+            "sidewinder" => Ok(Algorithm::Sidewinder),
+            _ => Err("no match"),
+        }
+    }
+}
+
+enum Output {
+    Ascii,
+}
+
+impl FromStr for Output {
+    type Err = &'static str;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "ascii" => Ok(Output::Ascii),
+            _ => Err("no match"),
+        }
+    }
+}
 fn main() {
     let yaml = load_yaml!("cli.yml");
     let matches = App::from_yaml(yaml).get_matches();
 
-    let algorithm = matches.value_of("algorithm").unwrap_or("sidewinder");
-    let output = matches.value_of("output").unwrap_or("ascii");
+    let algorithm = Algorithm::from_str(matches.value_of("algorithm").unwrap());
+    let output = Output::from_str(matches.value_of("output").unwrap());
 
     let mut grid = Grid::new(10, 10);
 
     match algorithm {
-        "binary" => binary_tree(&mut grid),
-        "sidewinder" => sidewinder(&mut grid),
-        _ => panic!("Invalid algorithm argument"),
+        Ok(Algorithm::Binary) => binary_tree(&mut grid),
+        Ok(Algorithm::Sidewinder) => sidewinder(&mut grid),
+        Err(_) => panic!("Invalid algorithm argument"),
     }
 
     match output {
-        "ascii" => {
+        Ok(Output::Ascii) => {
             print!("{}", grid.format());
         }
-        _ => panic!("Invalid output argument"),
+        Err(_) => panic!("Invalid output argument"),
     }
 }
