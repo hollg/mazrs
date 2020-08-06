24310783a8dec5dd1a8ea6a1e5efafc4b6707ef9
Gary Holland <garyhollandxyz@gmail.com>
2020-08-06 13:24:27 +0100

    rename var

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,23 @@
-use rand::Rng;
 extern crate rand;
+
+use rand::Rng;
 
 mod grid;
 use grid::{Cell, Grid};
 
 fn binary_tree(grid: &mut Grid) {
     grid.each_cell(|grid, cell| {
-        let mut cells: Vec<Cell> = Vec::new();
+        let mut north_and_east: Vec<Cell> = Vec::new();
 
         if cell.x < (grid.width - 1) {
-            cells.push(grid[cell.x + 1][cell.y].clone());
+            north_and_east.push(grid[cell.x + 1][cell.y].clone());
         }
         if cell.y < (grid.height - 1) {
-            cells.push(grid[cell.x][cell.y + 1].clone());
+            north_and_east.push(grid[cell.x][cell.y + 1].clone());
         }
 
-        if cells.len() > 0 {
-            grid.link(cell, rand::thread_rng().choose(&cells).unwrap())
+        if north_and_east.len() > 0 {
+            grid.link(cell, rand::thread_rng().choose(&north_and_east).unwrap())
         }
     })
 }
