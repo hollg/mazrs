27ecf7b41e6a44013d8e1f25c9e049f7b7acc2c3
Gary Holland <garyhollandxyz@gmail.com>
2020-08-06 21:36:01 +0100

    add clap for args parsing

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,31 @@
 mod generate;
 mod grid;
-
-// use generate::binary_tree::binary_tree;
+#[macro_use]
+extern crate clap;
+use clap::App;
+use generate::binary_tree::binary_tree;
 use generate::sidewinder::sidewinder;
 use grid::Grid;
 
 fn main() {
+    let yaml = load_yaml!("cli.yml");
+    let matches = App::from_yaml(yaml).get_matches();
+
+    let algorithm = matches.value_of("algorithm").unwrap_or("sidewinder");
+    let output = matches.value_of("output").unwrap_or("ascii");
+
     let mut grid = Grid::new(10, 10);
 
-    sidewinder(&mut grid);
-    print!("{}", grid.format());
+    match algorithm {
+        "binary" => binary_tree(&mut grid),
+        "sidewinder" => sidewinder(&mut grid),
+        _ => panic!("Invalid algorithm argument"),
+    }
+
+    match output {
+        "ascii" => {
+            print!("{}", grid.format());
+        }
+        _ => panic!("Invalid output argument"),
+    }
 }
