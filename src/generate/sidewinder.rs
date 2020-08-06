c5d7d5b557d1b3760bc174d6601e7338114fbb11
Gary Holland <garyhollandxyz@gmail.com>
2020-08-06 21:11:37 +0100

    remove unneeded code

--- src/generate/sidewinder.rs
+++ src/generate/sidewinder.rs
@@ -19,24 +19,17 @@ pub fn sidewinder(grid: &mut Grid) {
             let should_close_out =
                 at_eastern_boundary || !at_northern_boundary && rng.gen_range(0, 2) == 0;
 
-            let mut should_clear = false;
-
             if should_close_out {
                 let member = run.choose(&mut rng).unwrap();
                 if !at_northern_boundary {
                     let north_neighbour = grid[member.x][member.y + 1];
                     grid.link(member, &north_neighbour);
                 }
-                should_clear = true;
             } else {
                 let cell_a = grid[x][y];
                 let cell_b = grid[x + 1][y];
 
                 grid.link(&cell_a, &cell_b);
-            }
-
-            if should_clear {
-                run.clear();
             }
         }
     }
