0040cddbb21e6504b3491b11c82ff0dd5afb20aa
Gary Holland <garyhollandxyz@gmail.com>
2020-08-13 21:56:40 +0100

    use choose

--- src/generate/aldous_broder.rs
+++ src/generate/aldous_broder.rs
@@ -1,6 +1,7 @@
 extern crate rand;
 
-use rand::{thread_rng, Rng};
+use rand::seq::SliceRandom;
+use rand::thread_rng;
 
 use crate::types::grid::Grid;
 
@@ -10,13 +11,13 @@ pub fn generate(grid: &mut Grid) {
     let mut rng = thread_rng();
     while num_unvisited > 0 {
         let neighbours = grid.neighbours(cell);
-        let neighbour = neighbours[rng.gen_range(0, neighbours.len())];
+        let neighbour = neighbours.choose(&mut rng).unwrap();
 
         if !grid.links.contains_key(&(neighbour.x, neighbour.y)) {
             grid.link(&cell, &neighbour);
             num_unvisited -= 1;
         }
 
-        cell = neighbour;
+        cell = *neighbour;
     }
 }
