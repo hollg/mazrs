4a03bfa6024b31465820750e103e926b31ea95e9
Gary Holland <garyhollandxyz@gmail.com>
2020-08-13 08:21:27 +0100

    More wilson's progress

--- src/generate/wilson.rs
+++ src/generate/wilson.rs
@@ -5,6 +5,7 @@ use rand::seq::SliceRandom;
 use rand::{thread_rng, Rng};
 
 pub fn generate(grid: &mut Grid) {
+    // init
     let mut unvisited_cells: Vec<(usize, usize)> = vec![];
     let mut rng = thread_rng();
     for x in 0..grid.width {
@@ -16,33 +17,33 @@ pub fn generate(grid: &mut Grid) {
     let first_to_visit = unvisited_cells[rng.gen_range(0, unvisited_cells.len())];
     unvisited_cells.retain(|&coords| coords != first_to_visit);
 
+    // build path
     while unvisited_cells.len() > 0 {
-        let mut current_cell = unvisited_cells[rng.gen_range(0, unvisited_cells.len())];
-        let mut path: Vec<(usize, usize)> = vec![current_cell];
+        // let mut current_cell = unvisited_cells[rng.gen_range(0, unvisited_cells.len())];
+        let mut path: Vec<(usize, usize)> =
+            vec![unvisited_cells[rng.gen_range(0, unvisited_cells.len())]];
 
-        while unvisited_cells.contains(&current_cell) {
-            let neighbours = grid.neighbours(grid[current_cell.0][current_cell.1]);
-            let random_neighbour = neighbours.choose(&mut rand::thread_rng()).unwrap();
-            current_cell = (random_neighbour.x, random_neighbour.y);
+        while unvisited_cells.contains(path.last().unwrap()) {
+            let neighbours = grid.neighbours(grid[path.last().unwrap().0][path.last().unwrap().1]);
+            let random_neighbour = neighbours.choose(&mut rng).unwrap();
+            let target = (random_neighbour.x, random_neighbour.y);
 
-            match path.binary_search(&current_cell) {
+            match path.binary_search(&target) {
                 Ok(index) => {
-                    path = path[0..index + 1].to_vec();
+                    path.truncate(index + 1);
                 }
                 _ => {
-                    path.push(current_cell);
+                    path.push(target);
                 }
             }
         }
 
-        for i in 0..path.len() - 1 {
-            if let Some(j) = path.get(i + 1) {
-                let a = path[i];
-                let cell_a = grid[a.0][a.1];
-                let cell_b = grid[j.0][j.1];
-                grid.link(&cell_a, &cell_b);
-                unvisited_cells.retain(|&x| x != a);
-            }
+        // carve
+        for window in path.windows(2) {
+            let cell_a = grid[window[0].0][window[0].1];
+            let cell_b = grid[window[1].0][window[1].1];
+            grid.link(&cell_a, &cell_b);
+            unvisited_cells.retain(|&x| x != window[0] && x != window[1]);
         }
     }
 }
