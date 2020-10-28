fe81780ad26a11d536369172cd6cd88f00a76599
Gary Holland <gary@atomlearning.co.uk>
2020-10-28 16:10:19 +0000

    start on binary tree

--- mazrs_wasm/src/lib.rs
+++ mazrs_wasm/src/lib.rs
@@ -1,4 +1,4 @@
-use mazrs_lib::{generate::aldous_broder, types::grid::*};
+use mazrs_lib::{generate::binary_tree, types::grid::*};
 use wasm_bindgen::prelude::*;
 extern crate console_error_panic_hook;
 use std::panic;
@@ -7,7 +7,7 @@ use std::panic;
 pub fn generate() -> String {
     panic::set_hook(Box::new(console_error_panic_hook::hook));
     let mut grid = Grid::new(20, 20);
-    aldous_broder::generate(&mut grid);
+    binary_tree::generate(&mut grid);
 
     let svg = grid.to_svg();
     svg.to_string()
